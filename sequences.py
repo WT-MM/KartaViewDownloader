c23d6623c797adfa806998e2a16557cca00ab696
Wesley Maa <wesley.maa@gmail.com>
2021-06-09 20:24:12 -0700

    get sequences from node coords

--- sequences.py
+++ sequences.py
@@ -9,3 +9,20 @@ def getCoords(*args):
         return [(n[1]['x'], n[1]['y']) for n in list(osmnx.graph_from_place(query).nodes(data=True))]
     except ValueError as err:
         print("ValueError: ", err)
+        quit()
+
+def pullSequences(coords):
+    url = "http://openstreetcam.org/nearby-tracks"
+    data = {'lat': coords[1], 'lng' : coords[0], 'distance' : '10'}
+
+    r = requests.post(url=url, data=data)
+
+    sequences = []
+    osv = r.json()['osv']
+    if(type(osv) is dict):
+        for i in osv['sequences']:
+            sequences.append(i['sequence_id'])
+
+    return sequences
+
+
